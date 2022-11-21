d07d28818d0fffe704ba4188ce3778912a2eafcb
anoob <anoob@sina.cn>
2022-11-21 11:04:36 +0800

    feature:[crcommon]common

---
+++ crcommon/interface.h
@@ -0,0 +1,22 @@
+#ifndef CRCOMMON_INTERFACE
+#define CRCOMMON_INTERFACE
+#include "ccglobal/export.h"
+
+#if USE_CRCOMMON_DLL
+	#define CRCOMMON_API CC_DECLARE_IMPORT
+#elif USE_CRCOMMON_STATIC
+	#define CRCOMMON_API CC_DECLARE_STATIC
+#else
+	#if CRCOMMON_DLL
+		#define CRCOMMON_API CC_DECLARE_EXPORT
+	#else
+		#define CRCOMMON_API CC_DECLARE_STATIC
+	#endif
+#endif
+
+
+#include "trimesh2/TriMesh.h"
+#include <memory>
+
+
+#endif // CRCOMMON_INTERFACE
